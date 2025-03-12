00cd726fe6f46362949f74e2176cb9133e1b3bac
Hsin Kai <hsinkai2000@gmail.com>
2025-03-12 19:10:25 +0800

    Add Dockerfile and docker-compose files

--- asr/cv-decode.py
+++ asr/cv-decode.py
@@ -9,11 +9,19 @@ API_URL = "http://localhost:8001/asr"
 
 df = pd.read_csv(os.getenv("CSV_FILE_PATH"))
 
+# Loop through all rows in file
 for index, row in df.iterrows():
+
+    # Obtain audio file path
     curr_audio_file_path = os.path.join(
         os.getenv("AUDIO_FILE_PATH"), row['filename'])
+
+    # Read Audio file and make request to /asr endpoint
     with open(curr_audio_file_path, "rb") as audio_file:
         response = requests.post(API_URL, files={"file": audio_file})
+
+    # Append new col 'generated_text' at the end
     df.loc[index, "generated_text"] = response.json()["transcription"]
 
+# Save final file to current dir
 df.to_csv("cv-valid-dev-updated.csv", index=False)
